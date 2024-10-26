f574176e4ab33b640883659d0eab2c7ea9e12746
Onegodx <113604166+Onegodx@users.noreply.github.com>
2024-10-26 04:22:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DigitCountChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            bool result = HasSameDigitCount(number1, number2);
+
+            if (result)
+            {
+                Console.WriteLine("Числа имеют одинаковое количество цифр.");
+            }
+            else
+            {
+                Console.WriteLine("Числа имеют разное количество цифр.");
+            }
+        }
+
+        static bool HasSameDigitCount(int number1, int number2)
+        {
+            int digitCount1 = Math.Abs(number1).ToString().Length;
+            int digitCount2 = Math.Abs(number2).ToString().Length;
+
+            return digitCount1 == digitCount2;
+        }
+    }
+}
